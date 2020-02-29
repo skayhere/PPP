8391761ff1d5bf3c9e004b0e3eddc6201b994a80
Aishwarya Kulkarni <skay4kulkarni@gmail.com>
2020-02-29 18:02:54 +0530

    Add files via upload

--- mergeppp.c
+++ mergeppp.c
@@ -6,18 +6,18 @@
 #include <time.h>
 #include <sys/time.h>
 #define CHUNK_TH 10000 
-//performs the bottom up combine operation
+
 void merge(int a[], int l, int m, int r)
 {
 	int temp[m-l+1], temp2[r-m];
-	//copy first part to temporary array
+
 	for(int i=0; i<(m-l+1); i++)
 		temp[i]=a[l+i];
-	//copy second part to temporary arrat]y
+
 	for(int i=0; i<(r-m); i++)
 		temp2[i]=a[m+1+i];
 	int i=0, j=0, k=l;
-	//combine the arrays on the basis of order
+
 	while(i<(m-l+1) && j<(r-m))
 	{
 		if(temp[i]<temp2[j])
@@ -25,24 +25,18 @@ void merge(int a[], int l, int m, int r)
 		else
 			a[k++]=temp2[j++];
 	}
-	//to combine the remainder of the two arrays
+
 	while(i<(m-l+1))
 		a[k++]=temp[i++];
 	while(j<(r-m))
 		a[k++]=temp2[j++];
 
 }
-/* 
-This function is the main hotspot of the algorithm which I have chosen to parallelise
-The calls labelled call 1 and call 2 are essentially independent since they are operating on distinct
-parts of the array and hence can be called in parallel.
-*/
 
 void mergeSort(int a[], int l, int r)
 {
 	if(l<r)
 	{
-		//cout<<"came here";
 		int m=(l+r)/2;
 		#pragma omp parallel sections num_threads(2)
 		{
@@ -55,7 +49,7 @@ void mergeSort(int a[], int l, int r)
 				mergeSort(a,m+1,r); //call 2
 			}
 		}
-		merge(a,l,m,r); //this function is outside the parallel sections since it merges and access the same data 
+		merge(a,l,m,r);  
 	}
 }
 int main()
@@ -64,21 +58,22 @@ int main()
 	printf("Enter number of elements to sort : ");
 	scanf("%d",&len);
 	int arr[len];// = {0};
-	printf("Enter %d elements : \n",len);
+	//printf("Enter %d elements : \n",len);
 	for(int i=0; i<len; i++)
-		scanf("%d",&arr[i]);
-	omp_set_num_threads(4);
+		//scanf("%d",&arr[i]);
+		arr[i]=rand();
+	omp_set_num_threads(2);
         long start, end;
         struct timeval timecheck;
         gettimeofday(&timecheck, NULL);
         start = (long)timecheck.tv_sec * 1000 + (long)timecheck.tv_usec / 1000;
 
-	//call should be to a wrapper function that calls the mergeSort function
+
     
 	mergeSort(arr,0,len-1);
         gettimeofday(&timecheck, NULL);
         end = (long)timecheck.tv_sec * 1000 + (long)timecheck.tv_usec / 1000;
-        // printf("%ld milliseconds elapsed\n", (end - start));
+        printf("%ld milliseconds elapsed\n", (end - start));
         for (int i = 0; i < len; i++){
 	  	printf("%d ", arr[i]);
 	  }
